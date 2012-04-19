0f510a33994f4a1d7829b58aad200ed6f47ea3be
freezeflare <iceclimbers1990@gmail.com>
2012-04-19 11:08:16 -0400

    should be okay now

--- raytrace.h
+++ raytrace.h
@@ -18,4 +18,4 @@ struct R3Intersect
 
 
 R2Image *RenderImage(R3Scene *scene, int width, int height, int max_depth, 
-  int num_primary_rays_per_pixel, int num_distributed_rays_per_intersection);
+  int num_primary_rays_per_pixel, int num_distributed_rays_per_intersection, bool hard_shadow);
